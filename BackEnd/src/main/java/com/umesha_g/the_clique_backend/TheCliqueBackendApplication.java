f7bdeb03070e6cad93653b94dc6ccf0a6f7e55af
G Umesha Madushan <g.umesha.madushan@gmail.com>
2024-10-07 22:45:24 +0530

    initial Commit

---
+++ BackEnd/src/main/java/com/umesha_g/the_clique_backend/TheCliqueBackendApplication.java
@@ -0,0 +1,13 @@
+package com.umesha_g.the_clique_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCliqueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCliqueBackendApplication.class, args);
+	}
+
+}
---
+++ LICENSE.md
@@ -0,0 +1,3 @@
+Education Purpose Only. No Commercial Use.
+
+Copyright (c) 2024. Umesha G.
